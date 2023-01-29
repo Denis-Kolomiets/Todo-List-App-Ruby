4cab9a0c6d4baa3ae49cb9ac6c7876f3b0709fb2
Denis kolomiets <deniskolomiets988@mail.com>
2023-01-29 11:35:45 +0200

    Todo List App Ruby

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,45 @@
+class TodosController < ApplicationController
+    def index
+        @todos = Todo.all
+    end
+
+    def new 
+        @todo = Todo.new
+    end
+    def edit
+        binding.pry
+        @todo = Todo.find(params[:id])
+    end
+    def create
+        @todo = Todo.new(todo_params)
+        respond_to do |format|
+            if @todo.save
+                format.html {redirect_to root_path}
+            end
+        end
+    end
+
+    def update 
+       
+        @todo = Todo.find(params[:id])
+        respond_to do |format|
+            if @todo.update(todo_params)
+                format.html {redirect_to root_path}
+            end
+        end
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        if @todo.destroy
+            respond_to do |format|
+                format.html {redirect_to root_path}
+            end
+        end
+    end
+    
+    private
+    def todo_params
+        params.require(:todo).permit(:name, :completed)
+    end
+end
